38980746e769f8c3b6733bb86ae14053f089a50b
唐鑫阳 <azhvo730700@163.com>
2026-04-17 16:10:48 +0800

    build: 自动生成 UI Variables 和文件元数据
    DataTableGenerator 和 UIVariables Generator 生成的自动化文件。

---
+++ Assets/AAAGame/Scripts/UI/UIItemVariables/MapItemUI.Variables.cs
@@ -0,0 +1,15 @@
+//---------------------------------
+//此文件由工具自动生成,请勿手动修改
+//更新自:DESKTOP-7JQ11GM
+//---------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+public partial class MapItemUI
+{
+	[Space(10)]
+	[Header("UI Variables:")]
+	[SerializeField] private Button varBtn = null;
+	[SerializeField] private Text varMapName = null;
+	[SerializeField] private RectTransform varMask = null;
+	[SerializeField] private RectTransform varInteractUI = null;
+}
---
+++ Assets/AAAGame/Scripts/UI/UIVariables/OverworldUI.Variables.cs
@@ -0,0 +1,13 @@
+//---------------------------------
+//此文件由工具自动生成,请勿手动修改
+//更新自:DESKTOP-7JQ11GM
+//---------------------------------
+using UnityEngine;
+using UnityEngine.UI;
+public partial class OverworldUI
+{
+	[Space(10)]
+	[Header("UI Variables:")]
+	[SerializeField] private Button varCloseBtn = null;
+	[SerializeField] private GameObject varMapItemUI = null;
+}
