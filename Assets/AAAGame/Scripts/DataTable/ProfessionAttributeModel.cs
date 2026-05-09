3c67f9db16c80796f31000a8e72411da952195d5
唐鑫阳 <azhvo730700@163.com>
2026-05-09 18:25:00 +0800

    新增：消耗品、任务物品、宝物、职业模型配置表
    - 新增 ConsumableTable 消耗品配置表与代码
    - 新增 QuestItemTable 任务物品配置表与代码
    - 新增 TreasureTable 宝物配置表与代码
    - 新增 ProfessionAttributeModel 职业属性模型配置表与代码

---
+++ Assets/AAAGame/Scripts/DataTable/ProfessionAttributeModel.cs
@@ -0,0 +1,169 @@
+﻿//------------------------------------------------------------
+//------------------------------------------------------------
+// 此文件由工具自动生成，请勿直接修改。
+// 生成时间：__DATA_TABLE_CREATE_TIME__
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+#if ENABLE_OBFUZ
+[Obfuz.ObfuzIgnore(Obfuz.ObfuzScope.TypeName | Obfuz.ObfuzScope.MethodName)]
+#endif
+/// <summary>
+/// ProfessionAttributeModel
+/// </summary>
+public partial class ProfessionAttributeModel : DataRowBase
+{
+	private int m_Id = 0;
+	/// <summary>
+    /// 配置ID
+    /// </summary>
+    public override int Id
+    {
+        get { return m_Id; }
+    }
+
+        /// <summary>
+        /// 职业名称
+        /// </summary>
+        public string Profession
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 功能类型
+        /// </summary>
+        public string Function
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 品质等级
+        /// </summary>
+        public string Quality
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 最大生命值权重
+        /// </summary>
+        public double MaxHp
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 最大法力值权重
+        /// </summary>
+        public double MaxMp
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 护甲权重
+        /// </summary>
+        public double Armor
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 魔抗权重
+        /// </summary>
+        public double MagicResist
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 法术强度权重
+        /// </summary>
+        public double SpellPower
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 攻击力权重
+        /// </summary>
+        public double AtkDamage
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 说明
+        /// </summary>
+        public string Description
+        {
+            get;
+            private set;
+        }
+
+        public override bool ParseDataRow(string dataRowString, object userData)
+        {
+            string[] columnStrings = dataRowString.Split(DataTableExtension.DataSplitSeparators);
+            for (int i = 0; i < columnStrings.Length; i++)
+            {
+                columnStrings[i] = columnStrings[i].Trim(DataTableExtension.DataTrimSeparators);
+            }
+
+            int index = 0;
+            index++;
+            m_Id = int.Parse(columnStrings[index++]);
+            index++;
+            Profession = columnStrings[index++];
+            Function = columnStrings[index++];
+            Quality = columnStrings[index++];
+            MaxHp = double.Parse(columnStrings[index++]);
+            MaxMp = double.Parse(columnStrings[index++]);
+            Armor = double.Parse(columnStrings[index++]);
+            MagicResist = double.Parse(columnStrings[index++]);
+            SpellPower = double.Parse(columnStrings[index++]);
+            AtkDamage = double.Parse(columnStrings[index++]);
+            Description = columnStrings[index++];
+
+            return true;
+        }
+
+        public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(dataRowBytes, startIndex, length, false))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(memoryStream, Encoding.UTF8))
+                {
+                    m_Id = binaryReader.Read7BitEncodedInt32();
+                    Profession = binaryReader.ReadString();
+                    Function = binaryReader.ReadString();
+                    Quality = binaryReader.ReadString();
+                    MaxHp = binaryReader.ReadDouble();
+                    MaxMp = binaryReader.ReadDouble();
+                    Armor = binaryReader.ReadDouble();
+                    MagicResist = binaryReader.ReadDouble();
+                    SpellPower = binaryReader.ReadDouble();
+                    AtkDamage = binaryReader.ReadDouble();
+                    Description = binaryReader.ReadString();
+                }
+            }
+
+            return true;
+        }
+}
---
+++ Assets/AAAGame/Scripts/DataTable/TreasureTable.cs
@@ -0,0 +1,124 @@
+﻿//------------------------------------------------------------
+//------------------------------------------------------------
+// 此文件由工具自动生成，请勿直接修改。
+// 生成时间：__DATA_TABLE_CREATE_TIME__
+//------------------------------------------------------------
+
+using GameFramework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+#if ENABLE_OBFUZ
+[Obfuz.ObfuzIgnore(Obfuz.ObfuzScope.TypeName | Obfuz.ObfuzScope.MethodName)]
+#endif
+/// <summary>
+/// TreasureTable
+/// </summary>
+public partial class TreasureTable : DataRowBase
+{
+	private int m_Id = 0;
+	/// <summary>
+    /// 宝物表ID
+    /// </summary>
+    public override int Id
+    {
+        get { return m_Id; }
+    }
+
+        /// <summary>
+        /// 物品名称
+        /// </summary>
+        public string Name
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 对应ItemTable中的ID
+        /// </summary>
+        public int ItemTableId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 特殊效果ID
+        /// </summary>
+        public int SpecialEffectId
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 羁绊ID列表
+        /// </summary>
+        public int[] SynergyIds
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 基础属性(JSON格式)
+        /// </summary>
+        public string BaseAttributes
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 最大耐久度
+        /// </summary>
+        public int MaxDurability
+        {
+            get;
+            private set;
+        }
+
+        public override bool ParseDataRow(string dataRowString, object userData)
+        {
+            string[] columnStrings = dataRowString.Split(DataTableExtension.DataSplitSeparators);
+            for (int i = 0; i < columnStrings.Length; i++)
+            {
+                columnStrings[i] = columnStrings[i].Trim(DataTableExtension.DataTrimSeparators);
+            }
+
+            int index = 0;
+            index++;
+            m_Id = int.Parse(columnStrings[index++]);
+            Name = columnStrings[index++];
+            ItemTableId = int.Parse(columnStrings[index++]);
+            SpecialEffectId = int.Parse(columnStrings[index++]);
+            SynergyIds = DataTableExtension.ParseArray<int>(columnStrings[index++]);
+            BaseAttributes = columnStrings[index++];
+            MaxDurability = int.Parse(columnStrings[index++]);
+
+            return true;
+        }
+
+        public override bool ParseDataRow(byte[] dataRowBytes, int startIndex, int length, object userData)
+        {
+            using (MemoryStream memoryStream = new MemoryStream(dataRowBytes, startIndex, length, false))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(memoryStream, Encoding.UTF8))
+                {
+                    m_Id = binaryReader.Read7BitEncodedInt32();
+                    Name = binaryReader.ReadString();
+                    ItemTableId = binaryReader.Read7BitEncodedInt32();
+                    SpecialEffectId = binaryReader.Read7BitEncodedInt32();
+                    SynergyIds = binaryReader.ReadArray<int>();
+                    BaseAttributes = binaryReader.ReadString();
+                    MaxDurability = binaryReader.Read7BitEncodedInt32();
+                }
+            }
+
+            return true;
+        }
+}
